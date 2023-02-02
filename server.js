b1c0cdd34e4f17d4f9843e7124cd80e99446bc16
Akhtar Hassan <athz0905>
2023-02-02 09:28:22 +0530

    Added deployment scripts

--- server.js
+++ server.js
@@ -20,13 +20,15 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', userRoutes)
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
-
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
     app.use(express.static('client/build'));
 
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
   }
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
