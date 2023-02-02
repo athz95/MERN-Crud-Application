813425ba4a7a999de7ec9ee90685baab3ae82fcc
Akhtar Hassan <athz0905>
2023-02-02 08:52:19 +0530

    Added deployment scripts

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on 
 
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
-    app.use(express.static('./client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
   }
